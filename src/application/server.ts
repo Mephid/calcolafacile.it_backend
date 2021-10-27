7c6b5b91edb55b6031d30d3ac9406f484241fdfa
G. Cappai <cappaigianluca@gmail.com>
2021-10-28 01:36:10 +0200

    Fix typo

--- src/application/server.ts
+++ src/application/server.ts
@@ -3,7 +3,7 @@ import helmet from 'helmet'
 import cors from 'cors'
 
 import apiRoutes from './http/routes/api'
-import { handleHttpError } from './http/ErrorHandlers/handleHttpError'
+import { handleHttpError } from './http/errorHandlers/handleHttpError'
 
 const app = express()
 
